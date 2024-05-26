c49595b30f9aa75aa9ca432c373ebafee69f08a7
Jovanni_Ramirez <jovanni_r15@yahoo.com>
2024-05-25 23:56:30 -0700

    Added product class

---
+++ ts/main.ts
@@ -0,0 +1,14 @@
+
+/**Represents an individual products for sale */
+class Product {
+    //The name of the product
+    title: string;
+    
+    //The retail price of the product
+    price: number;
+
+    constructor(title: string, price: number) {
+        this.title = title;
+        this.price = price;
+    }
+}
